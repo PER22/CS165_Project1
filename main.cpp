4c9aa279d47243d698d3d1b18b5292f765f99dfd
Padraic Reilly <padraicreilly@Padraics-Air.lan>
2021-10-03 18:04:35 -0700

    filled out remainder of main() logic

--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
  
 using std::cout; using std::endl; using std::string;
 
-
-
+string MD5Crypt(string password, string salt, string magic){
+    string Alternate = md5(password + salt + password);
+    string Intermediate = md5(password + magic + salt  );//Incomplete logic here
+}
 
 
 int main(int argc, char *argv[])
 {
     char search_alphabet[] = {'a', 'b', 'c', 'd','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    string salt = "";
-    string correct_hash  = "";
+    string our_salt = "4fTgjp6q";
+    string our_correct_hash  = "pcnyPpGUQ2MvsGld8UMVa/";
     int cartesian_product_indices[6] = {0};
     while(cartesian_product_indices[0] < 26){
         //build search string
@@ -19,8 +21,9 @@ int main(int argc, char *argv[])
         for (int i = 0 ; i < 6; i++) {
             password_attempt += search_alphabet[cartesian_product_indices[i]];
         }
-        //TODO: MD5Crypt the password_attempt here, and compare it to correct_hash
-        cout<<password_attempt<<endl;
+        //md5Crypt hash our password attempt and compare it to the desired hash:
+        string hashed_password = MD5Crypt(password_attempt,our_salt,"$1$");
+        if(hashed_password == our_correct_hash){cout<<password_attempt; return 0;}
         //Increment the lsb of the indices list
         cartesian_product_indices[5]++;
         //Update the indices list 
@@ -32,6 +35,6 @@ int main(int argc, char *argv[])
         }
     }
     
-    //cout << "md5 of 'grape': " << md5("grape") << endl;
+    cout << "Failed to find password"<< endl;
     return 0;
 }
