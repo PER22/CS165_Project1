8946593358a39726d9bc83b5367447cb5c479f9d
Padraic Reilly <padraicreilly@Padraics-Air.lan>
2021-10-06 15:13:30 -0700

    First attempt at MD5crypt

--- main.cpp
+++ main.cpp
@@ -4,14 +4,27 @@
 using std::cout; using std::endl; using std::string;
 
 string MD5Crypt(string password, string salt, string magic){
-    string Alternate = md5(password + salt + password);
-    string Intermediate = md5(password + magic + salt  );//Incomplete logic here
+    MD5 alternate_md5 = MD5();
+    alternate_md5.update(password + salt + password);
+    string intermediate = md5(password + magic + salt + alternate.substr(0,6) + alternate[0] + char(00) + char(00));  
+    for (int i = 0; i < 1000; i++) {
+        string accumulator = "";
+        if(!(i%2)){accumulator += intermediate;}
+        else{accumulator += password;}
+        if(i%3){accumulator += salt;}
+        if(i%7){accumulator += password;}
+        if(!(i%2)){accumulator += password;}
+        else{accumulator += intermediate;}
+        intermediate = md5(accumulator);
+    }
+    cout<<intermediate<<endl;
+    return intermediate;
 }
 
 
 int main(int argc, char *argv[])
 {
-    char search_alphabet[] = {'a', 'b', 'c', 'd','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    string search_alphabet = "abcdefghijklmnopqrstuvwxyz";
     string our_salt = "4fTgjp6q";
     string our_correct_hash  = "pcnyPpGUQ2MvsGld8UMVa/";
     int cartesian_product_indices[6] = {0};
@@ -21,6 +34,7 @@ int main(int argc, char *argv[])
         for (int i = 0 ; i < 6; i++) {
             password_attempt += search_alphabet[cartesian_product_indices[i]];
         }
+        cout<<password_attempt<<endl;
         //md5Crypt hash our password attempt and compare it to the desired hash:
         string hashed_password = MD5Crypt(password_attempt,our_salt,"$1$");
         if(hashed_password == our_correct_hash){cout<<password_attempt; return 0;}
@@ -34,7 +48,6 @@ int main(int argc, char *argv[])
                 }
         }
     }
-    
     cout << "Failed to find password"<< endl;
     return 0;
 }
